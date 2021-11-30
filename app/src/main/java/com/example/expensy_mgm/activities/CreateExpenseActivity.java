bce8060207bad392c0e1cbd162ee9167c9be9f49
micah3252625 <micahmustaham@gmail.com>
2021-11-30 21:14:00 +0800

    Delete functionality implemented

--- app/src/main/java/com/example/expensy_mgm/activities/CreateExpenseActivity.java
+++ app/src/main/java/com/example/expensy_mgm/activities/CreateExpenseActivity.java
@@ -1,13 +1,17 @@
 package com.example.expensy_mgm.activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
@@ -31,6 +35,9 @@ public class CreateExpenseActivity extends AppCompatActivity {
 
     private Expense alreadyAvailableExpense;
     private ArrayAdapter<String> adapter;
+    private ImageView imageDelete;
+
+    private AlertDialog dialogDeleteExpense;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +54,13 @@ public class CreateExpenseActivity extends AppCompatActivity {
         inputExpenseDescription = findViewById(R.id.inputExpenseDescription);
         textDateTime = findViewById(R.id.textDateTime);
 
+        //
+        imageDelete = findViewById(R.id.imageDelete);
+        imageDelete.setOnClickListener(view -> {
+            showDeleteExpenseDialog();
+
+        });
+
         // set the textDateTime
         textDateTime.setText(
                 new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault())
@@ -56,6 +70,7 @@ public class CreateExpenseActivity extends AppCompatActivity {
         ImageView imageSave = findViewById(R.id.imageSave);
         imageSave.setOnClickListener(view -> saveExpense());
 
+        // sets the dropdown list
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, EXPENSES);
         inputExpenseCategory.setAdapter(adapter);
 
@@ -67,13 +82,19 @@ public class CreateExpenseActivity extends AppCompatActivity {
         }
     }
 
+    // this function sets handles the setting of viewing and updating the expense
     private void setViewOrUpdateExpense() {
         inputExpenseAmount.setText(Double.toString(alreadyAvailableExpense.getAmount()));
         inputExpenseCategory.setText(alreadyAvailableExpense.getCategory());
         inputExpenseDescription.setText(alreadyAvailableExpense.getDescription());
         textDateTime.setText(alreadyAvailableExpense.getDateTime());
+
+        // reinitialize the dropdown list
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, EXPENSES);
         inputExpenseCategory.setAdapter(adapter);
+
+        // show the delete icon when only viewing the expense
+        imageDelete.setVisibility(View.VISIBLE);
     }
 
     // function that parses the string to double
@@ -114,6 +135,7 @@ public class CreateExpenseActivity extends AppCompatActivity {
         expense.setDescription(inputExpenseDescription.getText().toString());
         expense.setDateTime(textDateTime.getText().toString());
 
+        // check if an expense instance exist
         if (alreadyAvailableExpense != null)
             expense.setId(alreadyAvailableExpense.getId());
 
@@ -139,6 +161,53 @@ public class CreateExpenseActivity extends AppCompatActivity {
 
     }
 
+    // function that handles the alert dialog when delete icon is clicked
+    private void showDeleteExpenseDialog() {
+        if (dialogDeleteExpense == null) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(CreateExpenseActivity.this);
+            View view = LayoutInflater.from(this).inflate(
+                    R.layout.layout_delete_expense,
+                    (ViewGroup) findViewById(R.id.layoutDeleteContainer)
+            );
+            builder.setView(view);
+            dialogDeleteExpense = builder.create();
+            if (dialogDeleteExpense.getWindow() != null) {
+                dialogDeleteExpense.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+            }
+            view.findViewById(R.id.textDeleteExpense).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    class DeleteExpenseTask extends AsyncTask<Void, Void, Void> {
+
+                        @Override
+                        protected Void doInBackground(Void... voids) {
+                            ExpensyDatabase.getExpensyDatabase(getApplicationContext()).expenseDao()
+                                    .deleteExpense(alreadyAvailableExpense);
+                            return null;
+                        }
+
+                        @Override
+                        protected void onPostExecute(Void aVoid) {
+                            super.onPostExecute(aVoid);
+                            Intent intent = new Intent();
+                            Toast.makeText(CreateExpenseActivity.this, "Expense Deleted!", Toast.LENGTH_SHORT).show();
+                            intent.putExtra("isExpenseDeleted", true);
+                            setResult(RESULT_OK, intent);
+                            finish();
+                        }
+                    }
+                    new DeleteExpenseTask().execute();
+                }
+            });
+            view.findViewById(R.id.textCancel).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialogDeleteExpense.dismiss();
+                }
+            });
+        }
+        dialogDeleteExpense.show();
+    }
 
     private static final String[] EXPENSES = new String[]{
             "Food", "Clothes", "Netflix", "Entertainment", "Gas", "Technology", "Miscellaneous"
